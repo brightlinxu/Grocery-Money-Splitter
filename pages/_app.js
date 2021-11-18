191249ab30b8409ffb112000be565e3687f77cc8
Bright Xu <brightxu@umich.edu>
2021-11-18 04:18:31 -0500

    fixed favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Groceries</title>
+        <link rel='shortcut icon' href='/favicon.ico' />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp
